f9f0dc292dd84c50c23b244bbdfba6af02a66357
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-08-07 10:54:12 -0400

    added notes

--- ex8.py
+++ ex8.py
@@ -1,9 +1,12 @@
+#This line is a Var called "Formatter"
 formattter = "{} {} {} {}"
 
+#I think these prints commands are using the () to fill in the {} via the string .format as an indicator
 print(formattter.format(1, 2, 3, 4))
 print(formattter.format("one", "two", "three", "four"))
 print(formattter.format(True, False, False, True))
 print (formattter.format(formattter, formattter, formattter, formattter))
+#I think the , is allowing the code to print on the same line
 print(formattter.format(
     "Try your",
     "Own text here",
