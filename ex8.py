cde3a1c451497c20f5a2ce4d88ce0201a4618171
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-08-07 10:56:45 -0400

    added note

--- ex8.py
+++ ex8.py
@@ -5,6 +5,7 @@ formattter = "{} {} {} {}"
 print(formattter.format(1, 2, 3, 4))
 print(formattter.format("one", "two", "three", "four"))
 print(formattter.format(True, False, False, True))
+# This prints 16 {} because the formatter is requested 4 times
 print (formattter.format(formattter, formattter, formattter, formattter))
 #I think the , is allowing the code to print on the same line
 print(formattter.format(
