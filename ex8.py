22f857cff9147abdb766b566f2f7bf0c7a9dd268
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-08-07 11:02:11 -0400

    added 5 requests per print . format

--- ex8.py
+++ ex8.py
@@ -1,17 +1,18 @@
 #This line is a Var called "Formatter"
-formattter = "{} {} {} {} {}"
+formattter = "{} {} {} {} {}" #{} must = # of print requests in .format
 
 #I think these prints commands are using the () to fill in the {} via the string .format as an indicator
-print(formattter.format(1, 2, 3, 4))
-print(formattter.format("one", "two", "three", "four"))
-print(formattter.format(True, False, False, True))
+print(formattter.format(1, 2, 3, 4, 5))
+print(formattter.format("one", "two", "three", "four", "five"))
+print(formattter.format(True, False, False, True, True))
 print(formattter.format("Green", "Yellow", "Blue","Orange", "Black"))
 # This prints 16 {} because the formatter is requested 4 times
-print (formattter.format(formattter, formattter, formattter, formattter))
+print (formattter.format(formattter, formattter, formattter, formattter, formattter))
 #I think the , is allowing the code to print on the same line
 print(formattter.format(
     "It is quite difficult to",
     "work on this while",
     "an intense conversation is happening behind me ",
-    "and Lucy is watching is Toy Story"
+    "and Lucy is watching is Toy Story",
+    "but i am still making progress!"
 ))
